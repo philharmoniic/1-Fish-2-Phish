a7168fb227686d5a6f8e6a77b902a2f75e69521a
Nathan W Wilson <72237980+NathanNWCW@users.noreply.github.com>
2025-02-22 14:18:12 -0500

    Update onephishtwophish.py

--- onephishtwophish.py
+++ onephishtwophish.py
@@ -10,7 +10,9 @@ os.environ["GOOGLE_API_KEY"] = st.secrets["key"]
 client = genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
-response = model.generate_content("You are a phishing scam email generator. Return an example phishing email from the Georgia Institute of Technology. Don't make it obviously a phishing email as this is part of a website about differentiating phishing from non-phishing.") #enter your prompt here!
+response = model.generate_content('''You are a phishing scam email generator. Return an example phishing email from the Georgia Institute of Technology. 
+Don't make it obviously a phishing email, and don't put a 'note: this is a fake phishing email' at the end. 
+This is part of a website about differentiating phishing from non-phishing.''') #enter your prompt here!
 print(response.text) #dont forget to print your response!
 
 st.title("One Phish Two Phish")
