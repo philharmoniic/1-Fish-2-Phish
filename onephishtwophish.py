b1c25d3135a1bff04778f6ca9c82c76c724e1030
Nathan W Wilson <72237980+NathanNWCW@users.noreply.github.com>
2025-02-22 14:14:36 -0500

    Update onephishtwophish.py

--- onephishtwophish.py
+++ onephishtwophish.py
@@ -6,7 +6,8 @@ import requests as req
 import google.generativeai as genai
 import os
 
-client = genai.configure(api_key="AIzaSyC4xfkOAnvagK_2j4L6YVZDJPouGS8JXlI")
+key = st.secrets("key")
+client = genai.configure(api_key=key)
 
 model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
 response = model.generate_content("You are a phishing scam email generator. Return an example phishing email from the Georgia Institute of Technology. Don't make it obviously a phishing email as this is part of a website about differentiating phishing from non-phishing.") #enter your prompt here!
