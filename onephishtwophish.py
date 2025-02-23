94fc12e9b6a8f0dc22dc702dfdf7477dbdda26f4
Nathan W Wilson <72237980+NathanNWCW@users.noreply.github.com>
2025-02-23 02:00:34 -0500

    Update onephishtwophish.py

--- onephishtwophish.py
+++ onephishtwophish.py
@@ -6,7 +6,7 @@ import requests as req
 import google.generativeai as genai
 import os
 
-os.environ["GOOGLE_API_KEY"] = st.secret['key']
+os.environ["GOOGLE_API_KEY"] = st.secrets['key']
 client = genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
