517061f4738c723dbe010982ee7de0f8ac83b5d7
Phillip Abraham <pjsabraham@gmail.com>
2025-02-23 03:23:49 -0500

    Update Phil_info.py

---
+++ Phil_info.py
@@ -0,0 +1,9 @@
+profile_picture = "static/P-photo.jpg"
+about_me = '''Hi! My name is Phil, and I'm a first year Computer Science major at the Georgia Institute of Technology! '''
+contributions = '''
+For ***1 Fish, 2 Phish***, I primarily worked on data cleaning, data processing, and the front-end of the project.
+
+I also designed the game's logo!
+'''
+
+my_github_url = "https://github.com/philharmoniic"
