323b9d212b4fd3f0fea67133694c449794afd14d
Phillip Abraham <pjsabraham@gmail.com>
2025-02-23 03:26:55 -0500

    Update 2_📄_Credits.py

--- pages/2_📄_Credits.py
+++ pages/2_📄_Credits.py
@@ -7,13 +7,13 @@ st.header("Who worked on ***1 Fish, 2 Phish***?", divider=True)
 
 st.subheader("Nathan Wilson")
 st.image(M.profile_picture, width=200)
-st.write(f"About Me: {M.about_me}\n")
-st.write(f"My Contributions: {M.contributions.strip()}\n")
-st.write(f"You can find my GitHub at [NathanNWCW]({M.my_github_url.strip()})!")
+st.write(f"**About Me:** {M.about_me}\n")
+st.write(f"**My Contributions:** {M.contributions.strip()}\n")
+st.write(f"You can find my **GitHub** at [NathanNWCW]({M.my_github_url.strip()})!")
 
 st.subheader("Phillip Abraham")
 st.image(P.profile_picture, width=200)
 st.write(f"**About Me:** {P.about_me}\n")
 st.write(f"**My Contributions:** {P.contributions.strip()}\n")
-st.write(f"You can find my GitHub at [philharmoniic]({P.my_github_url.strip()})!")
+st.write(f"You can find my **GitHub** at [philharmoniic]({P.my_github_url.strip()})!")
 
